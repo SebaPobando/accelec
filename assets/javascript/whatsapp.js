6bea69ca81bd422fc1c07a08981d0440edd40017
Sr Jengibre <spobleteobando@gmail.com>
2025-05-26 17:07:51 -0400

    ADD New section and change WhatsApp modal

--- assets/javascript/whatsapp.js
+++ assets/javascript/whatsapp.js
@@ -1,24 +1,24 @@
-/* BOTON WHATSAPP */
-const popupWhatsApp = () => {
-    let btnClosePopup = document.querySelector('.closePopup');
-    let btnOpenPopup = document.querySelector('.whatsapp-button');
-    let popup = document.querySelector('.popup-whatsapp');
-    let sendBtn = document.getElementById('send-btn');
+// /* BOTON WHATSAPP */
+// const popupWhatsApp = () => {
+//     let btnClosePopup = document.querySelector('.closePopup');
+//     let btnOpenPopup = document.querySelector('.whatsapp-button');
+//     let popup = document.querySelector('.popup-whatsapp');
+//     let sendBtn = document.getElementById('send-btn');
 
-    btnClosePopup.addEventListener("click", () => {
-        popup.classList.toggle('is-active-whatsapp-popup');
-    });
+//     btnClosePopup.addEventListener("click", () => {
+//         popup.classList.toggle('is-active-whatsapp-popup');
+//     });
 
-    btnOpenPopup.addEventListener("click", () => {
-        popup.classList.toggle('is-active-whatsapp-popup');
-        popup.style.animation = "fadeIn .6s 0.0s both";
-    });
+//     btnOpenPopup.addEventListener("click", () => {
+//         popup.classList.toggle('is-active-whatsapp-popup');
+//         popup.style.animation = "fadeIn .6s 0.0s both";
+//     });
 
-    sendBtn.addEventListener("click", () => {
-        let msg = document.getElementById('whats-in').value;
-        let relmsg = msg.replace(/ /g, "%20");
-        window.open('https://wa.me/56942054959?text=' + relmsg, '_blank');
-    });
-};
+//     sendBtn.addEventListener("click", () => {
+//         let msg = document.getElementById('whats-in').value;
+//         let relmsg = msg.replace(/ /g, "%20");
+//         window.open('https://wa.me/56942054959?text=' + relmsg, '_blank');
+//     });
+// };
 
-popupWhatsApp();
+// popupWhatsApp();
